9ab3ffe98b9ed9844ab03307a07fb49f944f8406
mtmazurik <marty@martymazurik.com>
2015-05-19 10:22:15 -0700

    add angular as external lib

--- helloangular/app.js
+++ helloangular/app.js
@@ -2,4 +2,7 @@ var app = angular.module('app', []);
 app.controller('theController', ['$scope',
     function($scope) {
         $scope.greeting = { text: "Hello Angular" };
+        $scope.handleClick = function() {
+            document.getElementById("lbl").innerHTML="Hello angular button-click";
+        }
     }]);
